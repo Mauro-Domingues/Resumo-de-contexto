613ef202bac7f5b223431d51e1bc60bf86d9dddf
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-07-21 16:34:28 -0300

    href-to-src

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@ const switcher = document.querySelector(".switcher");
 const style = document.querySelectorAll(".style");
 const styleMain = document.querySelector(".style-main")
 
-if(switcher.is(":checked")) {
-    style.href = "../css/dark.css";
-    styleMain.href = "./css/dark.css"
-    console.log("checked")
-} else {
-    style.href = "./css/light.css";
-    styleMain.href = "./css/light.css"
-    console.log("not checked")
-}
+switcher.addEventListener('change', () => {
+    if(switcher.checked) {
+        style.src = "../css/dark.css";
+        styleMain.src = "./css/dark.css"
+        console.log("checked")
+    } else {
+        style.src = "./css/light.css";
+        styleMain.src = "./css/light.css"
+        console.log("not checked")
+    }
+});
+
