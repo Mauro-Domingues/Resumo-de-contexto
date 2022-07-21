aead75ff03506867623e261a7db348128920ea8b
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-07-21 18:24:22 -0300

    silly-modifications

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 const switcher = document.querySelector(".switcher");
 const styleMain = document.head.getElementsByTagName('link');
+const styleSide = document.head.getElementsByTagName('link');
 
 switcher.addEventListener('change', () => {
     if(switcher.checked) {
         styleMain[1].href = "./css/light.css"
+        styleSide[3].href = "../css/light.css"
+
     } else {
         styleMain[1].href = "./css/dark.css"
+        styleSide[3].href = "../css/dark.css"
     }
 });
