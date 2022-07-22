4aea6b139e75aa5e06c6e099cc4d1637857c68cb
Mauro Domingues <104386910+Mauro-Domingues@users.noreply.github.com>
2022-07-21 21:24:21 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
 const switcher = document.querySelector(".switcher");
 const styleMain = document.head.getElementsByTagName('link');
-const styleSide = document.head.getElementsByTagName('link');
 
 switcher.addEventListener('change', () => {
     if(switcher.checked) {
         styleMain[1].href = "./css/light.css"
-        styleSide[3].href = "../css/light.css"
-
     } else {
         styleMain[1].href = "./css/dark.css"
-        styleSide[3].href = "../css/dark.css"
     }
-});
+}); // Preciso aplicar href="../css/light/dark.css" para as outras páginas do mesmo documento armazenando o estado do checkbox em cachê
